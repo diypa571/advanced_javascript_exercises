c2d8a573ed2657b420f1daab4710976cf959c7fb
Diyar P <diypa571@gmail.com>
2020-11-25 12:15:18 +0100

    Update loopunicode.js

--- loopunicode.js
+++ loopunicode.js
@@ -4,7 +4,7 @@ I allways forget to include charset="utf-8"
 */
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  alphabet.push("å"); // push a to alåha
+  alphabet.push("å"); // push a to the alphabet
   alphabet.push("ö");
   alphabet.push("ä");
   for (let i in alphabet) {
