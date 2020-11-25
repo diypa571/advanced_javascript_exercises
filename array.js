aeb6635d68428be111e596661b9f889f7c274285
Diyar P <diypa571@gmail.com>
2020-11-25 08:50:30 +0100

    Add files via upload

---
+++ array.js
@@ -0,0 +1,10 @@
+// Simple array
+const arraycities = ['Stockholm', 'Paris', 'Madrid', 'Rome'];
+//console.log(arraycities[2]);
+// Will return Madrid
+
+// Or simply Print all values of the array
+for(let a in arraycities) {
+    console.log(a); // Print the index
+  console.log(arraycities[a]);
+}
