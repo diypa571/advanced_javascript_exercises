bb86f4c59c48c34b8fc577dbdae7bcf346ee8a9c
Diyar P <diypa571@gmail.com>
2020-11-25 10:47:14 +0100

    Update prototype.js

--- prototype.js
+++ prototype.js
@@ -12,8 +12,8 @@ obj.d = 100; // Creating an object, property using an instance
 // How to crate a prototype???
 simplefunction.prototype.b = 11111000000; //  Creating a prototype...
 simplefunction.prototype.c = 101; //  Creating a prototype...
-simplefunction.prototype.e = 3.14000000; // This wont overwrite the prev "
-simplefunction.prototype.f =  19.1; // This wont overwrite the prev "
+simplefunction.prototype.e = 3.14000000; // This wont overwrite the prev  e
+simplefunction.prototype.f =  19.1; 
 
 
 console.log(obj.a);
