1e194763c24e262652b7ad609e23397321c81d72
Diyar P <diypa571@gmail.com>
2020-11-25 08:27:24 +0100

    Add files via upload

---
+++ creating_object.js
@@ -0,0 +1,12 @@
+// Creating an oject in Javascript
+const ObjectContianer = {
+Italay:"Rome",
+German:"Berlin",
+France:"Paris"
+};
+
+// A good way to print the object container is this.
+
+for (let c in ObjectContianer) {
+  console.log(c);
+}
