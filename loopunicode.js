d1819f335e24f57956c0634dce641eb4dacaa182
Diyar P <diypa571@gmail.com>
2020-11-25 11:06:00 +0100

    Update loopunicode.js

--- loopunicode.js
+++ loopunicode.js
@@ -1,3 +1,8 @@
+/*
+<script type="text/javascript" src="loopunicode.js" charset="utf-8"> </script>
+I allways forget to include charset="utf-8"
+*/
+
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 alphabet.push("å"); // push a to alåha
 alphabet.push("ö");
