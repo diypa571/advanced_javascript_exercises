d8eb24a97935576a0063c2dbbe046631f983e801
Diyar P <diypa571@gmail.com>
2020-11-25 10:46:44 +0100

    Add files via upload

---
+++ prototype.js
@@ -0,0 +1,24 @@
+//  Prototype
+// Functions has prototype property
+let simplefunction = function() {
+this.a = 10011;
+this.b = 10011000;
+this.e = 3.14;
+}
+
+
+let obj = new simplefunction();
+obj.d = 100; // Creating an object, property using an instance
+// How to crate a prototype???
+simplefunction.prototype.b = 11111000000; //  Creating a prototype...
+simplefunction.prototype.c = 101; //  Creating a prototype...
+simplefunction.prototype.e = 3.14000000; // This wont overwrite the prev "
+simplefunction.prototype.f =  19.1; // This wont overwrite the prev "
+
+
+console.log(obj.a);
+console.log(obj.b);
+console.log(obj.c);
+console.log(obj.d);
+console.log(obj.e);
+console.log(obj.f,obj);
