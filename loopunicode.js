9f5111c772e079e0fb31e14ca639d4ec63be9f78
Diyar P <diypa571@gmail.com>
2020-11-25 11:08:12 +0100

    Update loopunicode.js

--- loopunicode.js
+++ loopunicode.js
@@ -4,9 +4,10 @@ I allways forget to include charset="utf-8"
 */
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-alphabet.push("å"); // push a to alåha
-alphabet.push("ö");
-alphabet.push("ä");
-for (let i in alphabet) {
-  console.log(alphabet[i]);
+  alphabet.push("å"); // push a to alåha
+  alphabet.push("ö");
+  alphabet.push("ä");
+  for (let i in alphabet) {
+    console.log(alphabet[i]);
+     // console.log(alphabet[i].toUpperCase());   // To make the chars UpperCase
 }
